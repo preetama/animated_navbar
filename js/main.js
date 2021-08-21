0455ca10073675b2cce584cff9270393c2c29f09
preetama <preetam.satapathy007@gmail.com>
2021-08-21 23:35:32 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openFunction() {
+  document.getElementById("menu").style.width = "300px";
+  document.getElementById("mainbox").style.marginLeft = "300px";
+  document.getElementById("mainbox").innerHTML =
+    "Close";
+}
+function closeFunction() {
+  document.getElementById("menu").style.width = "0px";
+  document.getElementById("mainbox").style.marginLeft = "0px";
+  document.getElementById("mainbox").innerHTML = "&#9776; Open";
+}
